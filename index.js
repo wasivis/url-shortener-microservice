7a7df69554dc711179be8d70419eb1be79847d80
Wasivis <ivanmontequin@gmail.com>
2023-12-29 13:32:26 -0300

    Fix invalid url and short url bugs and add valid-url dependency

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const validUrl = require('valid-url');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -10,15 +11,21 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlMappings = {};
+let counter = 1;
 
 app.post('/api/shorturl', (req, res) => {
   const longUrl = req.body.url;
 
-  const shortUrl = `https://${req.get('host')}/api/shorturl/${Object.keys(urlMappings).length + 1}`;
+  if (!validUrl.isWebUri(longUrl)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  const shortUrl = `https://${req.get('host')}/api/shorturl/${counter}`;
 
   urlMappings[shortUrl] = longUrl;
+  counter++;
 
-  res.json({ original_url: longUrl, short_url: shortUrl });
+  res.json({ original_url: longUrl, short_url: counter - 1 });
 });
 
 app.get('/api/shorturl/:id', (req, res) => {
@@ -37,7 +44,6 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
