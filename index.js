eab9e2bf266e454b8dfb4ccdf252c3fe4fce0cb2
Wasivis <ivanmontequin@gmail.com>
2023-12-29 12:14:26 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const urlMappings = {};
+
+app.post('/api/shorturl', (req, res) => {
+  const longUrl = req.body.url;
+
+  const shortUrl = `https://${req.get('host')}/${Object.keys(urlMappings).length + 1}`;
+
+  urlMappings[shortUrl] = longUrl;
+
+  res.json({ original_url: longUrl, short_url: shortUrl });
+});
+
+app.get('/api/shorturl/:id', (req, res) => {
+  const id = req.params.id;
+  const shortUrl = `https://${req.get('host')}/${id}`;
+
+  if (urlMappings.hasOwnProperty(shortUrl)) {
+    res.redirect(urlMappings[shortUrl]);
+  } else {
+    res.json({ error: 'Short URL not found' });
+  }
+});
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
